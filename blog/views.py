c22a50cd47244522f093b828be75608261ee2b3c
CAUA RICARDO FLORES <05796690094@senacrs.edu.br>
2026-06-25 20:40:15 -0300

    Devblog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    mensagem = "<h1>Bem-vindo ao DevBlog!</h1> <p>Em brevem, artigos aqui.</p>"
+
+    return HttpResponse(mensagem)
+
+
+def sobre_nos(request):
+    mensagem = "<h1>Sobre o DevBlog</h1> <p>Mais informações em breve.</p>"
+
+    return HttpResponse(mensagem)
