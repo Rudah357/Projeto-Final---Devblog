ab38714318e15914d6ecd73c9452cff213d9c379
CAUA RICARDO FLORES <05796690094@senacrs.edu.br>
2026-07-01 21:13:29 -0300

    fazendo a pagina inicial sobre as noticias

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('sobre/', views.sobre_nos, name="sobre_nos")
+    path('sobre/', views.sobre_nos, name="sobre_nos"),
+    path('noticia/<int:id>/', views.detalhe_noticia, name='detalhe_noticia')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,35 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Artigo, Categoria
 
+def detalhe_noticia(request, id):
+    
+    noticia = get_object_or_404(Artigo, id=id)
+    lista_categorias = Categoria.objects.all()
+    
+    context = {
+        'noticia': noticia,
+        'lista_categorias': lista_categorias,
+    }
+    return render(request, 'blog/detalhe.html', context)
 
 def home(request):
+    categoria_id = request.GET.get('categoria')
 
-    return render(request, 'blog/index.html')
+    noticias = Artigo.objects.all().order_by('-data_publicacao')
+    categorias = Categoria.objects.all()
 
+    if categoria_id:
+            noticias = noticias.filter(categoria_id=categoria_id)
+
+    lista_categorias = Categoria.objects.all()
+
+    contexto = {
+        'lista_artigos': noticias,
+        'lista_categorias': categorias
+    }
     
+    return render(request, 'blog/index.html', contexto)
+
 def sobre_nos(request):
 
     return render(request, 'blog/sobre.html')
