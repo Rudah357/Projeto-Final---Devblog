5d80f19a406418dd29652b9db4a0e00d347ed6bc
CAUA RICARDO FLORES <05796690094@senacrs.edu.br>
2026-07-02 21:24:03 -0300

    checkpoint 7

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Artigo, Categoria
 
+def lista_artigos(request):
+    lista_artigos = Artigo.objects.all()
+    categoria_selecionada = None
+
+    categoria_slug = request.GET.get('categoria')
+
+    if categoria_slug:
+        categoria_selecionada = get_object_or_404(Categoria, slug=categoria_slug)
+        lista_artigos = lista_artigos.filter(categoria=categoria_selecionada)
+
+    context = {
+        'lista_artigos': lista_artigos,
+        'categoria_atual': categoria_selecionada,
+    }
+    return render(request, 'blog/seu_template.html', context)
+
 def detalhe_noticia(request, id):
     
     noticia = get_object_or_404(Artigo, id=id)
@@ -17,15 +33,16 @@ def home(request):
 
     noticias = Artigo.objects.all().order_by('-data_publicacao')
     categorias = Categoria.objects.all()
+    categoria_selecionada = None
 
     if categoria_id:
-            noticias = noticias.filter(categoria_id=categoria_id)
-
-    lista_categorias = Categoria.objects.all()
+        categoria_selecionada = get_object_or_404(Categoria, id=categoria_id)
+        noticias = noticias.filter(categoria_id=categoria_id)
 
     contexto = {
         'lista_artigos': noticias,
-        'lista_categorias': categorias
+        'lista_categorias': categorias,
+        'categoria_atual': categoria_selecionada,
     }
     
     return render(request, 'blog/index.html', contexto)
