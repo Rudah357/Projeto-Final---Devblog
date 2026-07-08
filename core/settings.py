2322993feffb585dc8783b86b6c137697a31b91f
CAUA RICARDO FLORES <05796690094@senacrs.edu.br>
2026-07-08 20:58:48 -0300

    embelezando o contato

--- core/settings.py
+++ core/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -136,3 +137,5 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACESS_TOKEN_LIFETIME': timedelta(days=1)
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
